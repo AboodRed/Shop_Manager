09f41d7a7e3e9cf8f8b8e8f77572377f7bf61a58
bilal1992 <bilal.m2011@gmail.com>
2012-06-02 13:21:12 +0300

    server gui
    تم انجاز 50% من الواجهات حيث تو انجاز واجهة اللوك ان و اليوزير

---
+++ admin_shwping/src/admin_shwping/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package admin_shwping;
+
+/**
+ *
+ * @author bilal
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
