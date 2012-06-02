51f6102abd2780a66ef073713fa2529022faf97a
AboodRed <abdelrahman.hamra@gmail.com>
2012-06-02 12:13:21 +0300

    initialize project

---
+++ shop_Manager/src/shop_manager/Shop_Manager.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shop_manager;
+
+/**
+ *
+ * @author iPod
+ */
+public class Shop_Manager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
